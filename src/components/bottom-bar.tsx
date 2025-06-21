10839e553d0c5c45f9a1adff8b061ad62519a515
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-06-22 01:25:19 +0530

    added tooltips for bottom navigation and adjusted paddings a bit

--- src/components/bottom-bar.tsx
+++ src/components/bottom-bar.tsx
@@ -1,19 +1,45 @@
 import { BadgePlusIcon, HouseIcon, UserIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import type { ReactElement } from "react";
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "@/components/ui/tooltip";
 
 function BottomBarComponent(): ReactElement {
   return (
-    <div className="flex flex-row justify-between items-center">
-      <Button variant="ghost" size="icon" className="size-12">
-        <HouseIcon className="size-8" />
-      </Button>
-      <Button variant="ghost" size="icon" className="size-12">
-        <BadgePlusIcon className="size-8" />
-      </Button>
-      <Button variant="ghost" size="icon" className="size-12">
-        <UserIcon className="size-8" />
-      </Button>
+    <div className="flex flex-row justify-between items-center p-1">
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="ghost" size="icon" className="size-12">
+            <HouseIcon className="size-8" />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Home</p>
+        </TooltipContent>
+      </Tooltip>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="ghost" size="icon" className="size-12">
+            <BadgePlusIcon className="size-8" />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Create</p>
+        </TooltipContent>
+      </Tooltip>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="ghost" size="icon" className="size-12">
+            <UserIcon className="size-8" />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Profile</p>
+        </TooltipContent>
+      </Tooltip>
     </div>
   );
 }
